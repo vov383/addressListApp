35515ec2ffac5f8d41a19aa8e06e0d31b37ba09e
dawool <dawool@DESKTOP-SE93QV5>
2024-02-20 13:01:01 +0900

    초기 커밋.

---
+++ addressListApp/CommMysql.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace Dawool
+{
+    class CommMysql
+    {
+        private static string ConnectionString()
+        {
+             return @"SERVER=192.168.0.180;DATABASE=employee_list;UID=root;PASSWORD=dw#1234;"; 
+        }
+
+        #region [Method] ExecuteDataSet
+        public static DataSet ExecuteDataSet(string sbSql)
+        {
+            DataSet ds = new DataSet();
+            
+
+            string connectionString = ConnectionString();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+                string commandText = sbSql;
+                MySqlCommand cmd = new MySqlCommand(commandText, conn);
+
+                adapter.SelectCommand = cmd;
+                adapter.Fill(ds, "rtn_table");
+                conn.Close();
+                return ds;
+            }
+            catch (MySqlException ex)
+            {
+                //MessageBox.Show("DATABASE 연결이 되지 않습니다. 다시 확인해 주세요.");
+                //Environment.Exit(-1);
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+        #endregion
+            
+        #region [Method] ExecuteNonQueryc
+        public static int ExecuteNonQuery(string sbSql)
+        {
+            int intRtn = -1;
+            DataSet ds = new DataSet();
+
+            string connectionString = ConnectionString();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+
+                string commandText = sbSql;
+                MySqlCommand cmd = new MySqlCommand(commandText, conn);
+
+                intRtn = cmd.ExecuteNonQuery();
+                conn.Close();
+                return intRtn;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("DATABASE 연결이 되지 않습니다. 다시 확인해 주세요.");
+                Environment.Exit(-1);
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ addressListApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace addressListApp
+{
+    public partial class winFormApp : Form
+    {
+        string _id = "root";
+        string _pw = "dw#1234";
+        string _database = "employee_list";
+        string _server = "192.168.0.180";
+        string _connectionAddress = "";
+        string _port = "3306";
+
+        public winFormApp()
+        {
+            InitializeComponent();
+
+            _connectionAddress = string.Format("SERVER={0};PORT={1};DATABASE={2};UID={3};PASSWORD={4};", _server, _port, _database, _id, _pw, _database);
+
+            selectAll();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            selectAll();
+        }
+
+        private void selectAll()
+        {
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+
+                    // 전체 데이터를 조회합니다.          
+                    string selectQuery = string.Format("SELECT * FROM employee_list");
+
+                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
+                    MySqlDataReader table = command.ExecuteReader();
+
+                    listView1.Items.Clear();
+
+                    while (table.Read())
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.Text = table["id"].ToString();
+                        item.SubItems.Add(table["emp_name"].ToString());
+                        listView1.Items.Add(item);
+
+
+                    }
+
+                    table.Close();
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void winFormApp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        
+    }
+}
